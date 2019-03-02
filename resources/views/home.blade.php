4ff657a21c1cbf4673d2121e5af9c63ba5bb00ef
jazzankan <shasfn@gmail.com>
2019-03-02 16:21:38 +0100

    Model Project med controller. 2 projekt skapade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
             <div class="card">
                 <div class="card-header">Dashboard</div>
                 <div class="card-body">
-                    <p><a href="http://phpmyadmin.app" target="_blank">phpmyadmin</a> med hemligt lösenord</p>
+                    <p><a href="https://phpmyadmin.app" target="_blank">phpmyadmin</a> med hemligt lösenord</p>
                     <hr>
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
