0d1cb6a90ae75e0a2555232620f609884635f7e6
jazzankan <shasfn@gmail.com>
2019-03-09 15:19:50 +0100

    show project

--- resources/views/projects/list.blade.php
+++ resources/views/projects/list.blade.php
@@ -5,7 +5,7 @@
         <h1>Mina projekt</h1>
         <ul class="list-group">
             @foreach ($projectlist as $project)
-                <li class="list-group-item">{{ $project->title }}</li>
+                <li class="list-group-item"><h4><a href="/projects/{{ $project->id }}">{{ $project->title }}</a></h4></li>
             @endforeach
         </ul>
     <div>
---
+++ resources/views/projects/show.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>{{ $project->title }}</h1>
+        <p><span class="font-weight-bold">Beskrivning:</span><br>
+        {{ $project->description }}</p>
+        <p><span class="font-weight-bold">Deadline:</span> {{ $project->deadline }}</p>
+        <div>
+@endsection
