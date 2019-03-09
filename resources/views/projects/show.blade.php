d68ea3512264e2435b8e503899d4b40be5405fd4
jazzankan <shasfn@gmail.com>
2019-03-09 15:29:10 +0100

    show project 2

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <div class="container">
         <h1>{{ $project->title }}</h1>
+        <hr>
         <p><span class="font-weight-bold">Beskrivning:</span><br>
         {{ $project->description }}</p>
         <p><span class="font-weight-bold">Deadline:</span> {{ $project->deadline }}</p>
