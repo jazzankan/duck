dd5146d5031ba51d70c19671b6543301ef6bb53e
jazzankan <shasfn@gmail.com>
2020-02-12 11:06:50 +0100

    Börjat med frontend för söknng filtrereing av minnen

--- resources/views/memories/list.blade.php
+++ resources/views/memories/list.blade.php
@@ -3,7 +3,12 @@
     <div class="container">
         @if(count($memories) > 0)
             <h1>Minnen</h1>
-            <p><a href="/memories/create" class="btn btn-primary btn-sm">Nytt minne</a></p>
+            <form class="form-inline" method="post" action="{{ route('memories.store') }}">
+                @csrf
+            <a href="/memories/create" class="btn btn-primary btn-sm newmem">Nytt minne</a>
+                <input type="text" class="form-control newmembox" value="{{ old('search') }}" name="search"/> <button type="submit" class="btn btn-primary newmem">Sök</button>
+                <a href="#" v-on:click="filter = true"><b>Filtrera</b></a>
+            </form>
             <ul class="list-group striped-list">
                 @foreach ($memories as $memory)
                     <li class="list-group-item"><h4><a href="/memories/{{ $memory->id }}">{{ $memory->title }}</a></h4></li>
