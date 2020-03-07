8c74b3c77ab755d1b9385c29ac21a808b12ec8a3
jazzankan <shasfn@gmail.com>
2020-03-07 17:28:44 +0100

    Lista med arbetsuppgifter förhoppningsvis färdig.

--- resources/views/todos/list.blade.php
+++ resources/views/todos/list.blade.php
@@ -3,10 +3,11 @@
 @section('content')
     <div class="container">
         @if(count($undonetodos) > 0)
-            <h1>Ofärdiga Arbetsuppgifter</h1>
+            <h1>Ofärdiga arbetsuppgifter</h1>
             <ul class="list-group striped-list">
                 @foreach ($undonetodos as $t)
-                    <li class="list-group-item"><h4>{{ $t->title }}</h4> @if($t['deadline']) Deadline: <span @if($t['deadline'] <= $today)class="redalert"@endif>{{ $t->deadline }}</span>@endif</li>
+                    <li class="list-group-item"><h4>{{ $t->title }}</h4> @if($t['deadline']) Deadline: <span @if($t['deadline'] <= $today)class="redalert"@endif>{{ $t->deadline }}</span>@endif<br>
+                        <h5>Tillhör projekt: <a href="/projects/{{ $t->project_id }}">{{ $t->projname }}</a></h5></li>
                 @endforeach
             </ul>
         @else
