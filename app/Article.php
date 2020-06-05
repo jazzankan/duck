726fe8735c5534d1f08e6b17e339c63531e73b2f
jazzankan <shasfn@gmail.com>
2020-06-05 19:02:25 +0200

    Skapat nya modellen Comment, med controller och migration. Har kanske gjort färdigt för migration.

--- app/Article.php
+++ app/Article.php
@@ -12,4 +12,8 @@ class Article extends Model
     {
         return $this->belongsTo('App\Category');
     }
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
 }
