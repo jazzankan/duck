ecaaeb42c24c97b33fcd87000206ac097bb74f78
jazzankan <shasfn@gmail.com>
2020-06-07 10:53:59 +0200

    Täppt till hålet att oinloggade kunde komma åt att skiva blogginlägg eller skapa kategorier. Lagt till konstruktor till controllers. Även deployat till ank.webbsallad.se

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -13,6 +13,12 @@ class ArticleController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $articles = Article::orderByDesc('updated_at')->paginate(6);
--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -12,6 +12,11 @@ class CommentController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $comments = Comment::all()->sortBy('created_at');
